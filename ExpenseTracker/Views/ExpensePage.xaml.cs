e416a318e7dcc25514891608caf878b7c9dc3482
harshithavaas <harshitha.vaas@gmail.com>
2022-11-16 21:59:49 -0800

    Expense Page updates - Adjusted vertical spacing - Added colors to headers and buttons - Button name changes to Update , Delete when expense is loaded from main page

--- ExpenseTracker/Views/ExpensePage.xaml.cs
+++ ExpenseTracker/Views/ExpensePage.xaml.cs
@@ -16,122 +16,126 @@ namespace ExpenseTracker.Views
     public partial class ExpensePage : ContentPage
     {
         public String SelectedCategory { get; set; }
-   
-    public ExpensePage()
-    {
-        InitializeComponent();
-    }
 
-    protected override void OnAppearing()
-    {
-        SelectedCategory = Category.Miscellaneous.ToString();
-        var expense = (Expense)BindingContext;
-        Date.Date = DateTime.UtcNow;
-        if (expense != null)
+        public ExpensePage()
         {
-            ExpenseName.Text = expense.Name;
-            Amount.Text = expense.Amount.ToString();
-            Date.Date = expense.Date;
-            SetSelectedRadioCategory(expense.Category);
-
+            InitializeComponent();
         }
-    }
 
-    private void SetSelectedRadioCategory(Category category)
-    {
-        switch (category.ToString())
+        protected override void OnAppearing()
         {
-            case "Food":
-                rbFood.IsChecked = true;
-                break;
-            case "Housing":
-                rbHousing.IsChecked = true;
-                break;
-            case "Travel":
-                rbTravel.IsChecked = true;
-                break;
-            case "Colthing":
-                rbColthing.IsChecked = true;
-                break;
-            case "PetCare":
-                rbPetCare.IsChecked = true;
-                break;
-            case "Miscellaneous":
-                rbMiscellaneous.IsChecked = true;
-                break;
-        }
-    }
-
-    private void Amount_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        var textbox = (Entry)sender;
-        if (textbox != null)
-        {
-            var tempValue = decimal.Parse(textbox.Text, CultureInfo.InvariantCulture);
-            var newFormat = tempValue.ToString("N2", CultureInfo.InvariantCulture);
-            textbox.Text = newFormat;
-            textbox.CursorPosition = newFormat.Length - 3;
-        }
-    }
-
-    private void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (verifyRequiredFeilds())
-        {
+            SelectedCategory = Category.Miscellaneous.ToString();
             var expense = (Expense)BindingContext;
-            if (expense == null || string.IsNullOrEmpty(expense.FileName))
+
+            if (expense != null)
             {
-                expense = new Expense();
-                expense.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                    $"{Path.GetRandomFileName()}.notes.txt");
+                ExpenseName.Text = expense.Name;
+                Amount.Text = expense.Amount.ToString();
+                Date.Date = expense.Date;
+                SetSelectedRadioCategory(expense.Category);
+
+                SaveButton.Text = "Update";
+                CancelButton.Text = "Delete";
+                CancelButton.Background = Xamarin.Forms.Brush.DarkSalmon;
+
+            }
+        }
+
+        private void SetSelectedRadioCategory(Category category)
+        {
+            switch (category.ToString())
+            {
+                case "Food":
+                    rbFood.IsChecked = true;
+                    break;
+                case "Housing":
+                    rbHousing.IsChecked = true;
+                    break;
+                case "Travel":
+                    rbTravel.IsChecked = true;
+                    break;
+                case "Colthing":
+                    rbColthing.IsChecked = true;
+                    break;
+                case "PetCare":
+                    rbPetCare.IsChecked = true;
+                    break;
+                case "Miscellaneous":
+                    rbMiscellaneous.IsChecked = true;
+                    break;
+            }
+        }
+
+        private void Amount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textbox = (Entry)sender;
+            if (textbox != null && !string.IsNullOrEmpty(textbox.Text))
+            {
+                var tempValue = decimal.Parse(textbox.Text, CultureInfo.InvariantCulture);
+                var newFormat = tempValue.ToString("N2", CultureInfo.InvariantCulture);
+                textbox.Text = newFormat;
+                textbox.CursorPosition = newFormat.Length - 3;
+            }
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            if (verifyRequiredFeilds())
+            {
+                var expense = (Expense)BindingContext;
+                if (expense == null || string.IsNullOrEmpty(expense.FileName))
+                {
+                    expense = new Expense();
+                    expense.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        $"{Path.GetRandomFileName()}.notes.txt");
+                }
+
+                string TotalFileText = $"{ExpenseName.Text};{Amount.Text};{Date.Date};{SelectedCategory}";
+
+                File.WriteAllText(expense.FileName, TotalFileText);
+
+                Navigation.PopModalAsync();
             }
 
-            string TotalFileText = $"{ExpenseName.Text};{Amount.Text};{Date.Date};{SelectedCategory}";
+        }
 
-            File.WriteAllText(expense.FileName, TotalFileText);
+        private bool verifyRequiredFeilds()
+        {
+            List<string> required = new List<string>();
+            if (string.IsNullOrEmpty(ExpenseName.Text))
+                required.Add("Expense Name");
 
+            if (Amount.Text == null)
+                required.Add("Amount");
+
+            if (required.Count > 0)
+            {
+                string missingFeilds = string.Join(", ", required);
+                ErrorMessage.Text = $"Enter {missingFeilds}";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            var expense = (Expense)BindingContext;
+            if (expense != null && File.Exists(expense.FileName))
+            {
+                File.Delete(expense.FileName);
+            }
+            ExpenseName.Text = string.Empty;
+            Amount.Text = String.Empty;
+            Date.Date = DateTime.Now;
+            rbMiscellaneous.IsChecked = true;
             Navigation.PopModalAsync();
         }
 
-    }
-
-    private bool verifyRequiredFeilds()
-    {
-        List<string> required = new List<string>();
-        if (string.IsNullOrEmpty(ExpenseName.Text))
-            required.Add("Expense Name");
-
-        if (Amount.Text == null)
-            required.Add("Amount");
-
-        if (required.Count > 0)
+        private void ExpenseCategory_CheckedChanged(object sender, CheckedChangedEventArgs e)
         {
-            string missingFeilds = string.Join(", ", required);
-            ErrorMessage.Text = $"Enter {missingFeilds}";
-            return false;
+            RadioButton button = sender as RadioButton;
+            SelectedCategory = button.Value.ToString();
         }
-
-        return true;
     }
-
-    private void CancelButton_Clicked(object sender, EventArgs e)
-    {
-        var expense = (Expense)BindingContext;
-        if (expense != null && File.Exists(expense.FileName))
-        {
-            File.Delete(expense.FileName);
-        }
-        ExpenseName.Text = string.Empty;
-        Amount.Text = null;
-        Date.Date = DateTime.Now;
-        rbMiscellaneous.IsChecked = true;
-        Navigation.PopModalAsync();
-    }
-
-    private void ExpenseCategory_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        RadioButton button = sender as RadioButton;
-        SelectedCategory = button.Value.ToString();
-    }
-}
 }
